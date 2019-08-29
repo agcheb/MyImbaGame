d47bba07f3666f8cb2c7808916fdb7a6847015d2
agcheb <agchebykin@gmail.com>
2019-08-29 23:27:37 +0300

    Initial commit

---
+++ android/src/com/alextiersenmatuwa/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.alextiersenmatuwa.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.alextiersenmatuwa.game.MyImbaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyImbaGame(), config);
+	}
+}
---
+++ desktop/src/com/alextiersenmatuwa/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alextiersenmatuwa.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alextiersenmatuwa.game.MyImbaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyImbaGame(), config);
+	}
+}
